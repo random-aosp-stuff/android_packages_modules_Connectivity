48d2527e435600d18d409879f1fd65c3d1444c0b
Remi NGUYEN VAN <reminv@google.com>
2024-07-03 18:50:03 +0900

    Add flag for QUEUE_CALLBACKS_FOR_FROZEN_APPS
    Add a flag to be used for an optimization to queue callbacks while apps
    are frozen, instead of filling the binder buffer.
    
    Bug: 327038794
    Bug: 279392981
    Test: atest
    Change-Id: I6cc2507de88fae3d0866f9831e1e9026200297e4

--- tests/unit/java/com/android/server/connectivityservice/base/CSTest.kt
+++ tests/unit/java/com/android/server/connectivityservice/base/CSTest.kt
@@ -165,6 +165,7 @@ open class CSTest {
         it[ConnectivityFlags.BACKGROUND_FIREWALL_CHAIN] = true
         it[ConnectivityFlags.DELAY_DESTROY_SOCKETS] = true
         it[ConnectivityFlags.USE_DECLARED_METHODS_FOR_CALLBACKS] = true
+        it[ConnectivityFlags.QUEUE_CALLBACKS_FOR_FROZEN_APPS] = true
     }
     fun setFeatureEnabled(flag: String, enabled: Boolean) = enabledFeatures.set(flag, enabled)
 
